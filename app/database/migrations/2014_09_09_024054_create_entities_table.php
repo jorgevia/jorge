59410913b2a584e66c78b3002ee094f5e8eab399
Jorge Viale Desktop 8.1 <viale.jorge@gmail.com>
2014-09-29 21:06:20 -0300

    database entities modified

--- app/database/migrations/2014_09_09_024054_create_entities_table.php
+++ app/database/migrations/2014_09_09_024054_create_entities_table.php
@@ -14,12 +14,28 @@ class CreateEntitiesTable extends Migration {
 	{
 		Schema::create('entities', function(Blueprint $table)
 		{
-			$table->increments('id');
+			$table->increments('id_entities'); //Primary key
 
-			$table->string('first_name', 150);
-			$table->string('middle_name', 150);
-			$table->string('last_name', 150);
+			$table->char('name', 255)->unique(); //Add index to the na
+			$table->string('description')->nullable();
 
+			$table->string('email')->nullable();
+			$table->string('address')->nullable();
+
+			//Google maps
+			$table->string('latitude')->nullable();
+			$table->string('longitude')->nullable();
+
+			//Foreign keys
+			$table->tinyInteger('id_zone')->unsigned()->default(1);
+			$table->tinyInteger('id_target')->unsigned()->default(1)->nullable(); //If teachs kids, adults, etc.
+			$table->smallInteger('id_type')->unsigned();
+
+			$table->smallInteger('id_country')->unsigned()->default(1);
+			$table->smallInteger('id_province')->unsigned()->default(1);
+			$table->smallInteger('id_city')->unsigned()->default(1);
+
+			//Se va a permitir agregar una imagen más tarde
 			$table->timestamps();
 		});
 	}
