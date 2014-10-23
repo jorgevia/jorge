686acd9ceec7a3a4c7bbd38184bbc7bc71633778
Jorge Viale Desktop 8.1 <viale.jorge@gmail.com>
2014-10-23 09:00:35 -0300

    directive learning

--- public/views/guide/guide.js
+++ public/views/guide/guide.js
@@ -11,25 +11,38 @@ angular.module('myApp.guideEngine', ['ngRoute'])
 
 .controller('guideCtrl', ['$scope', function($scope) {
     $scope.title = "Buenos Aires Tango Guide";
+
+    //This is retrieved by a service
     $scope.countries = [
         {value: '1', description: 'Argentina'},
         {value: '2', description: 'Perú'}
     ];
 
-    $scope.loadCountries = function() {
-        console.log("called");
-    };
-    $scope.loadProvinces = function() {
-    };
-
 
 }])
-    .directive("selectguide", function() {
+    .directive("selectGuide", function() {
     return {
-        restrict: "A",
+        restrict: "E",
         //scope: {},
         templateUrl: 'views/guide/templates/guide-finder.html',
+        scope: {
+            list: "=", //Acá decimos que list está ligado con el objeto con el nombre de la propiedad
+            label: "@",
+            type: "@"
+        },
+        controller: function($scope) {
+            //$scope.country = 1;
+            $scope.dataChanged = function() {
+                console.log("Data cambiada");
+                console.log($scope.country);
+                //Acá deberíamos llamar a la otra directiva para que se refresque
+            };
+        },
         link: function (scope, element, attrs) {
-            scope.type = attrs.selectguide;
+            console.log(attrs);
+            console.log(scope.list);
+            console.log(scope.label);
+            console.log(scope.type);
+            //Sets default country
         }
-    }});
+}});
